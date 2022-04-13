37cab4c8dfbbf511f38f2830fba59c415bf1de97
Romain SALVAN <romain.salvan@g-mail.fr>
2022-04-13 21:02:24 +0200

    typo fix and spec for singletons

--- Design Patterns/Creational/Singleton/PreLoadedSingleton/src/Government.java
+++ Design Patterns/Creational/Singleton/PreLoadedSingleton/src/Government.java
@@ -1,11 +1,11 @@
 public class Government {
-    private static final Government instance = new Government();
+    private static final Government INSTANCE = new Government();
 
     private Government() {
     }
 
     public static Government getInstance() {
-        return instance;
+        return INSTANCE;
     }
 
     public void doMeasure(String measure){
